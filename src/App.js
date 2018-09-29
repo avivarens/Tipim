bfc976629a9e050a1b8e6cd0f38153cba1fe2a39
avivarens <avivar6193@gmail.com>
2018-09-29 14:25:42 +0300

    setting invironment

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import DatePicker from './DatePicker';
+import 'tachyons';
+
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      //should contain: (date, shift, adj, tipsInput)
+      
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title tc">Tipim</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          
         </p>
       </div>
     );
