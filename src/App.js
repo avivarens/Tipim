7dd7339e2a9c1591573ea85ef4a7db1098fd6218
avivarens <avivar6193@gmail.com>
2018-09-30 16:20:59 +0300

    adding DatePicker component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React, { Component } from 'react';
-import DatePicker from './DatePicker';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+import 'react-datepicker/dist/react-datepicker.css';
+import PropTypes from 'prop-types'; // ES6
 import 'tachyons';
 
+// class App extends Component {
+//   constructor() {
+//     super()
+//     this.state = {
+//       //should contain: (DatePicker, shift, adj, tipsInput)
+//     }
+//   }
 
-class App extends Component {
-  constructor() {
-    super()
+  class App extends React.Component {
+  constructor (props) {
+    super(props)
     this.state = {
-      //should contain: (date, shift, adj, tipsInput)
-      
-    }
+      startDate: moment()
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+ 
+  handleChange(date) {
+    this.setState({
+      startDate: date
+    });
   }
 
   render() {
     return (
-      <div className="App">
+      <div className="App tc">
         <header className="App-header">
-          <h1 className="App-title tc">Tipim</h1>
+          <h1 className="App-title">Tipim</h1>
         </header>
         <p className="App-intro">
-          
+          Comming Soon...
         </p>
+        <DatePicker 
+          dateFormat="DD/MM/YYYY"
+          isClearable={true}
+          selected={this.state.startDate}
+          onChange={this.handleChange}
+        />
       </div>
     );
   }
