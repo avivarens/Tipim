ac5b331e5066e0ae6dd1dd6f13d7798f9a8ef5f5
avivarens <avivar6193@gmail.com>
2018-10-02 15:32:28 +0300

    seperating datepicker component

--- src/App.js
+++ src/App.js
@@ -1,49 +1,21 @@
 import React, { Component } from 'react';
-import DatePicker from 'react-datepicker';
-import moment from 'moment';
-import 'react-datepicker/dist/react-datepicker.css';
-import PropTypes from 'prop-types'; // ES6
+import DatePicker from './DatePicker';
 import 'tachyons';
 
-// class App extends Component {
-//   constructor() {
-//     super()
-//     this.state = {
-//       //should contain: (DatePicker, shift, adj, tipsInput)
-//     }
-//   }
-
-  class App extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      startDate: moment()
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
- 
-  handleChange(date) {
-    this.setState({
-      startDate: date
-    });
-  }
-
-  render() {
-    return (
-      <div className="App tc">
-        <header className="App-header">
-          <h1 className="App-title">Tipim</h1>
-        </header>
-        <p className="App-intro">
-          Comming Soon...
-        </p>
-        <DatePicker 
-          dateFormat="DD/MM/YYYY"
-          isClearable={true}
-          selected={this.state.startDate}
-          onChange={this.handleChange}
-        />
-      </div>
+  class App extends Component {
+    render() {
+      return (
+        <div className="App tc">
+          <header className="App-header">
+            <h1 className="App-title">Tipim</h1>
+          </header>
+          <p className="App-intro">
+            Comming Soon...
+          </p>
+          <div>
+          <DatePicker />
+          </div>
+        </div>
     );
   }
 }
---
+++ src/DatePicker.js
@@ -0,0 +1,34 @@
+import React, {componentt} from 'react';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+import 'react-datepicker/dist/react-datepicker.css';
+ 
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      startDate: moment()
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+ 
+  handleChange(date) {
+    this.setState({
+      startDate: date
+    });
+  }
+ 
+  render() {
+    return (
+    <DatePicker
+      key='date' 
+      dateFormat="DD/MM/YYYY"
+      isClearable={true}
+      selected={this.state.startDate}
+      onChange={this.handleChange}
+      />
+    )  
+  }
+}
+
+export default DatePicker;
